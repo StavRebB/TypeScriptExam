5cbf23ec9edfcfc3fd35a6a7137fe146ef72c596
StavRebB <stavrebb@yahoo.com>
2021-01-28 14:20:14 +0200

    SOLUTION FOR EXAM IS READY

--- solution.js
+++ solution.js
@@ -1,4 +1,9 @@
 console.log("STARTING PROJECT");
+var listDiv = document.querySelector('#todo-list');
+var addBtn = document.querySelector('#todo-save');
+var taskInput = document.querySelector('#todo-item');
+var submitBtn = document.querySelector('#todo-add');
+var deleteAll = document.querySelector('#todo-delall');
 var Task = /** @class */ (function () {
     function Task(todo) {
         this.todo = todo;
@@ -9,7 +14,40 @@ var Task = /** @class */ (function () {
 var randKey = function () {
     return Math.ceil(Math.random() * 100000);
 };
-var submitBtn = document.getElementById('todo-add');
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
 });
+deleteAll.addEventListener('click', function () {
+    localStorage.clear();
+    listDiv.innerHTML = "";
+});
+addBtn.addEventListener('click', function () {
+    var newItem = new Task(taskInput.value);
+    var newName = "Obj_" + newItem.id;
+    localStorage.setItem(newName, JSON.stringify(newItem));
+    console.log(newItem);
+    createNewItem(newItem);
+});
+var createNewItem = function (obj) {
+    var para = document.createElement('div');
+    para.classList.add('todo-item');
+    para.innerText = obj.todo;
+    para.setAttribute('id', "Obj_" + obj.id);
+    var newBtn = document.createElement('input');
+    newBtn.classList.add('todo-ok');
+    newBtn.setAttribute('type', 'button');
+    newBtn.setAttribute('value', '✓');
+    newBtn.setAttribute('onclick', 'markTask(event)');
+    para.append(newBtn);
+    listDiv.append(para);
+};
+window.onload = function () {
+    for (var i = 0; i < localStorage.length; i++) {
+        var tasking = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        createNewItem(tasking);
+    }
+};
+var markTask = function (event) {
+    var btnDiv = event.target.parentElement;
+    btnDiv.classList.add('done');
+};
--- solution.ts
+++ solution.ts
@@ -1,5 +1,11 @@
 console.log("STARTING PROJECT")
 
+const listDiv = document.querySelector('#todo-list')
+const addBtn = document.querySelector('#todo-save')
+const taskInput = document.querySelector('#todo-item')
+const submitBtn = document.querySelector('#todo-add')
+const deleteAll = document.querySelector('#todo-delall')
+
 class Task {
     public todo:string
     public id:number
@@ -14,8 +20,58 @@ let randKey = ():number => {
     return Math.ceil(Math.random() * 100000)
 }
 
-const submitBtn = document.getElementById('todo-add')
-
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault()
 })
+
+deleteAll.addEventListener('click', () => {
+    localStorage.clear()
+    listDiv.innerHTML = ""
+})
+
+addBtn.addEventListener('click', () => {
+    let newItem = new Task(taskInput.value)
+
+    let newName:string = `Obj_${newItem.id}`
+
+    localStorage.setItem(newName,JSON.stringify(newItem))
+
+    console.log(newItem)
+
+    createNewItem(newItem)
+})
+
+let createNewItem = (obj) => {
+    let para = document.createElement('div')
+
+    para.classList.add('todo-item')
+
+    para.innerText = obj.todo;
+
+    para.setAttribute('id',`Obj_${obj.id}`)
+
+    let newBtn = document.createElement('input')
+
+    newBtn.classList.add('todo-ok')
+
+    newBtn.setAttribute('type','button')
+
+    newBtn.setAttribute('value','✓')
+
+    newBtn.setAttribute('onclick','markTask(event)')
+
+    para.append(newBtn)
+    listDiv.append(para)
+}
+
+window.onload = () => {
+    for (var i = 0; i < localStorage.length; i++){
+        let tasking = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        createNewItem(tasking)
+    }
+}
+
+let markTask = (event) => {
+    let btnDiv = event.target.parentElement
+    btnDiv.classList.add('done')
+}
